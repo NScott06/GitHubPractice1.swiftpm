36efa300f863f508c11e1a3a341eeaf06cc7bd2a
Nicholas Scott <nscott4525@stu.d214.org>
2023-04-04 14:49:45 -0500

    added vars to store numbers and added textfields to enter numbers to use to calculate

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number1:Int = 0
+    @State var number2:Int = 0
     @State var ans:Int = 0
     var body: some View {
+        HStack{
+            Group{
+                TextField("Enter your first number", value: $number1, format: .number)
+                TextField("Enter your second number", value: $number2, format: .number)
+            }
+            .padding()
+            .textFieldStyle(.roundedBorder)
+        }
         VStack {
             Button {
                 
